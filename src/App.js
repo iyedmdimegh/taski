8543b0f48daf67895403e874b703e22237d14b4e
iyedmdimegh <iyedmdimegh@gmail.com>
2023-11-29 02:12:36 +0100

    styled a bit and added a sign now working on tasklist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
 import Intro from './components/Intro/Intro'
+import Sign from './components/Sign/Sign'
+
+const tasksList=[
+  {
+    id:"id1",
+    name:"Do dishes",
+    date: new Date(2023, 11,18),
+  },
+  {
+    id:"id2",
+    name:"Go to school",
+    date: new Date(2023, 9,4),
+  },
+  {
+    id:"id3",
+    name:"Study",
+    date: new Date(2023, 10,28),
+  }
+]
+
+
+
 function App() {
   return (
     <div className="App">
       <Intro/>
+      <Sign className="sign-pos"/>
     </div>
   );
 }
